c5ed631882ecd4e6c7ea3c694dd552168261db8e
syss <syss@sys>
2024-10-15 17:58:39 +0530

    code002

---
+++ src/Reliance.java
@@ -0,0 +1,188 @@
+
+public class Reliance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String companyname="*********"
+				+ "RELIANCE INSURANCE LTD********"; 
+		String companydeatails="company details";
+		String companyaddress1="1st street cross cut";
+		String companyaddress2="Gandipuram";
+        String companyaddress3="Coimbatore";
+        String companyaddress4="641027";
+        String phonenumber="9994455668";
+        String Gstno="123333356478956";    
+        String customerdetails="premium customer details";
+        int totalnooftwowheelercustomer=25000000;
+		int totalnooffourwheelercustomer=2000000;
+		int totalnoofhealthinsrancecustomer=7800000;
+		int totalnooftravelinusrancecustomer=5630000; 
+        String policyscheme="policy scheme";
+        String policy1="twowheeler";
+        String policy2="fourwheeler";
+        String policy3="healthinsrance";
+        String policy4="travelinusrance";
+		String Premium="premium value";
+		int twowheelerpremium=2500;
+		int fourwheelerpremium=4500;
+		int healthinsrancepremium=5500;
+		int travelinusrancepremium=1500;
+		String policydurationpackage="1year";		
+		System.out.println("***************************************************************************");
+		System.out.println(companyname);
+		System.out.println(companydeatails);
+		System.out.println("company address1:"+companyaddress1);
+		System.out.println("company address2:"+companyaddress2);
+		System.out.println("company address3:"+companyaddress3);
+		System.out.println("phone number:"+phonenumber);
+		System.out.println("Gstno:"+Gstno);
+		System.out.println("****************************************************************************");
+		System.out.println(customerdetails);
+		System.out.println("Total No of Two wheeler Customer:"+totalnooftwowheelercustomer);
+		System.out.println("Total No of Four Wheeler Customer:"+totalnooffourwheelercustomer);
+		System.out.println("Total No of Health Insrance Customer:"+totalnoofhealthinsrancecustomer);
+		System.out.println("Total No of Travel Inusrance Customer:"+totalnooftravelinusrancecustomer);
+		System.out.println("****************************************************************************");
+		System.out.println(policyscheme);
+		System.out.println("policy1:"+policy1);
+		System.out.println("policy2:"+policy2);
+		System.out.println("policy3:"+policy3);
+		System.out.println("policy4:"+policy4);
+		System.out.println("Premium:"+Premium);
+		System.out.println("Two wheeler premium:"+twowheelerpremium);
+		System.out.println("Four wheeler premium:"+fourwheelerpremium);
+		System.out.println("Health insrance premium:"+healthinsrancepremium);
+		System.out.println("Travel inusrance premium:"+travelinusrancepremium);
+		System.out.println("Policy duration package:"+policydurationpackage);
+		System.out.println("***************************************************************************");
+		String details="policy holder detail";
+		String policyholdername="aradhya";
+		String poilcyholderaddress1="No5 LG nagar";
+		String poilcyholderaddress2="near sss school";
+		String poilcyholderaddress3="singanallur";
+		String poilcyholderaddress4="coimbatore";
+		String poilcyholderaadhano="1234 6549 7897";
+		String policyholderdob="15.3.1997";
+		String policyholderage="26";
+		String policyholderphoneno="6380996690";
+		String policyname="two wheeler insurance policy";
+		String policyno="98765123444448";
+		String poilcyclaimno="555566669999777";
+		String premiumamount="2500";
+		String insurancestartingdate="16.05.2023";
+		String insurancesendingdate="17.05.2024";
+		boolean insuranceclaimduringpoilcyperiod=true;
+		System.out.println(details);
+		System.out.println("Policy holder Name:"+policyholdername);
+		System.out.println("Poilcy holder Address1:"+poilcyholderaddress1);		
+		System.out.println("Poilcy holder Address2:"+poilcyholderaddress2);
+		System.out.println("Poilcy holder Address3:"+poilcyholderaddress3);
+		System.out.println("Poilcy holder Address4:"+poilcyholderaddress4);
+		System.out.println("Poilcy holder Aadhar no:"+poilcyholderaadhano);
+		System.out.println("Policy holder DOB:"+policyholderdob);
+		System.out.println("Policy holder Phone no:"+policyholderphoneno);
+		System.out.println("Policy name:"+policyname);
+		System.out.println("Policy no:"+policyno);
+		System.out.println("Poilcy claim no:"+poilcyclaimno);
+		System.out.println("Premium amount:"+premiumamount);
+		System.out.println("Insurance starting date:"+insurancestartingdate);
+		System.out.println("Insurancesendingdate:"+insurancesendingdate);
+		System.out.println("Insurance claim during poilcy period:"+insuranceclaimduringpoilcyperiod);
+		System.out.println("**********************************************************************");
+		String employeedetails="employeedetails";
+		String employeename="vishnu";
+		String employeeno="65676677"; 
+		String employeesalarylpa="450000";
+		String employeesalarypermonth="34000";
+		String employeepfno="7894561236455";
+		String employeeesino="8888555642";
+		String employeeaadharno="6547 7895 1246";
+		String employeebankaccountno="77778844542215";
+		String employeephoneno="9566573654";
+		String employeeaddress1="No5 ram nagar";
+		String employeeaddress2="snr school";
+		String employeeaddress3="gandipuram";
+		String employeeaddress4="coimbatore";
+		String employeeaddress5="641006";
+		String employeeholderdob="17.5.1992";
+		String employeeage="32";
+		System.out.println(employeedetails);
+		System.out.println("Employee Name:"+employeename);
+		System.out.println("Employee No:"+employeeno);
+		System.out.println("Employee Salary lpa:"+employeesalarylpa);
+		System.out.println("Employee salary per month:"+employeesalarypermonth);
+		System.out.println("Employee PF No:"+employeepfno);
+		System.out.println("Employee ESI No:"+employeeesino);
+		System.out.println("Employee Aadhar No:"+employeeaadharno);
+		System.out.println("Employeebank Account No:"+employeebankaccountno);
+		System.out.println("Employee Phone No:"+employeephoneno);
+		System.out.println("Employee Address1:"+employeeaddress1);
+		System.out.println("Employee Address2:"+employeeaddress2);
+		System.out.println("Employee Address3:"+employeeaddress3);
+		System.out.println("Employee Address4:"+employeeaddress4);
+		System.out.println("Employee Address5:"+employeeaddress5);
+		System.out.println("Employee Holder dob:"+employeeholderdob);
+		System.out.println("Employee Age:"+employeeage);
+		System.out.println("******************************************************************");
+		
+		
+				
+		
+		
+		
+		
+		
+						
+		
+		
+		
+		
+		
+				
+		
+		
+	}	
+
+	}
+
+
+
+
+	
+
+		
+		
+		
+				
+		
+		
+		
+
+	
+
+
+
+
+
+		
+
+
+
+
+
+				
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
